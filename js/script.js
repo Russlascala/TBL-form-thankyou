e99eca4725f9c4cd530a34857d16135089f6c4b2
Russ Lascala <russlascala@gmail.com>
2018-09-06 11:26:29 -0400

    finished first page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+
+
+/* Script to change the column stacking for mobile to get list on bottom of form */
+$(window).resize(function () {
+    console.log('resize called');
+    var width = $(window).width();
+    /*  log the width to find out what what pixel to change the order at.
+        The dev tools in browser might be different */
+    //console.log(width); 
+    if (width <= 752) {
+        $('.topMobile').addClass('order-first');
+        $('.bottomMobile').addClass('order-last');
+    } else {
+        $('.topMobile').removeClass('order-first');
+        $('.bottomMobile').removeClass('order-last');
+    } 
+})
+    .resize();//trigger the resize event on page load.
+
+
+
+
 // When the user clicks on the button, scroll to the top of the document
 function scrollToTop(scrollDuration) {
     var scrollStep = -window.scrollY / (scrollDuration / 15),
